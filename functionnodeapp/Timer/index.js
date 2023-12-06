b80bb8420e956cc96c4ef44184f65bb6820e58cb
access <roy.gebrayel11@gmail.com>
2023-12-06 23:29:22 +0200

    s

--- functionnodeapp/Timer/index.js
+++ functionnodeapp/Timer/index.js
@@ -1,9 +1,25 @@
 module.exports = async function (context, myTimer) {
-    var timeStamp = new Date().toISOString();
-    
-    if (myTimer.isPastDue)
-    {
-        context.log('JavaScript is running late!');
-    }
-    context.log('JavaScript timer trigger function ran!', timeStamp);   
+  if (myTimer.isPastDue) {
+    context.log("JavaScript is running late!");
+  }
+
+  context.log("Hello, I'm the trigger!");
+
+  // Display the HTML message
+  context.res = {
+    headers: {
+      "Content-Type": "text/html",
+    },
+    body: "<html><body><h1>Hello, I'm the trigger!</h1></body></html>",
+  };
+
+  // Set a timeout to clear the message after 5 seconds
+  setTimeout(() => {
+    context.res = {
+      status: 204,
+      body: "",
+    };
+    context.log("Message cleared.");
+    context.done();
+  }, 5000);
 };
